8975ab52ed740b320b648a5454bbf25fa2b4f300
Mark Hsu <valac.barca@gmail.com>
2020-10-09 21:35:58 +0800

    add header

--- products.py
+++ products.py
@@ -10,3 +10,14 @@ print(products)
 
 for p in products:
 	print(p[0],"的價格是",p[1],"元")
+
+with open ("products.csv", "w", encoding = "utf-8") as f:   #給了檔名、寫入模式、編碼
+	f.write("商品,價格\n")
+	for p in products:
+		f.write(p[0]+","+ p[1]+"\n")
+
+
+
+
+
+
