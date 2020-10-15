2d637d8c648b4b3c8795ac42d52707e78f11be48
ValacBarca <70145571+ValacBarca@users.noreply.github.com>
2020-10-15 16:02:17 +0800

    Update products.py

--- products.py
+++ products.py
@@ -4,12 +4,12 @@ import os
 def read_file(filename):
 	products = []
 	with open (filename, "r", encoding = "utf-8") as f: 
-				for line in f:
-					if "商品,價格" in line:
-						continue #繼續
-					name,price = line.strip().split(",")
-					products.append([name, price])
-				print(products)
+		for line in f:
+			if "商品,價格" in line:
+				continue #繼續
+			name,price = line.strip().split(",")
+			products.append([name, price])
+		print(products)
 	return products
 
 #讓使用者輸入
